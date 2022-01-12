802c8a483129b8f9bd4138055492ce03af2b071d
shun1121 <hikitanikun.ware80000@gmail.com>
2022-01-12 17:38:50 +0900

    implemented select and count function

--- popup.js
+++ popup.js
@@ -2,7 +2,9 @@ const button = document.getElementById('btn')
 console.log(button)
 button.addEventListener("click", () => {
   console.log('working fine')
+  chrome.storage.sync.get('num', (item) => {
+    const number = document.getElementById('value_num').innerHTML = item.num
+    console.log(number)
+  })
 })
-
-
 
---
+++ select.js
@@ -0,0 +1,7 @@
+document.addEventListener('selectionchange', () => {
+  const selected_val = window.getSelection().toString()
+  const val_length = selected_val.length
+  chrome.storage.sync.set({num: val_length}, () => {
+    console.log('Value_length is ' + val_length)
+  })
+})
