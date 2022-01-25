333c1bd97bf91f58f3d4a5c88f56f79163a967c2
shun1121 <hikitanikun.ware80000@gmail.com>
2022-01-25 15:36:20 +0900

    deleted debug

--- select.js
+++ select.js
@@ -1,8 +1,6 @@
 document.addEventListener('selectionchange', () => {
   const selected_val = window.getSelection().toString()
   const val_length = selected_val.length
-  chrome.storage.sync.set({num: val_length}, () => {
-    console.log('Value_length is ' + val_length)
-  })
+  chrome.storage.sync.set({num: val_length}, () => {})
 })
 
